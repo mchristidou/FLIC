3289d76c2979d2af763a850c4ca7203a4c02d21c
mchristidou03 <mxristidou63@gmail.com>
2025-07-20 15:00:02 +0300

    changed js file

--- header.php
+++ header.php
@@ -18,7 +18,7 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.min.js"></script> 
     <link rel="stylesheet" href="https://code.jquery.com/ui/1.13.2/themes/smoothness/jquery-ui.css">
-    <script src="validation.js" type="text/javascript"></script>
+    <script src="functions.js" type="text/javascript"></script>
 </head>
 
 <body class="d-flex flex-column min-vh-100" style="background: linear-gradient(to bottom,rgb(182, 187, 252) ,rgb(248, 244, 255));">
