a2c51075489f710ec29ba217dc27089a109d1b64
mchristidou03 <mxristidou63@gmail.com>
2025-10-02 15:03:27 +0300

    tiny fix - star rating order

--- movie_page.php
+++ movie_page.php
@@ -9,8 +9,6 @@
     require('nav.php');
     require('functions.php');
 
-    $apiKey = "a42405df"; //OMDB API key
-
     $movie_id = $_GET['movie_id'];
     $sql = "SELECT * FROM movies WHERE movie_id = :movie_id";
     $query = $db->prepare($sql);
@@ -110,13 +108,13 @@
                                 <form action="add_review.php" method="get">
                                     <!-- STAR RATING -->
                                     <div class="star-rating mb-3">
-                                        <input type="radio" id="star1" name="rating" value="1">
+                                        <input type="radio" id="star1" name="rating" value="5">
                                         <label for="star1">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                         </label>
-                                        <input type="radio" id="star2" name="rating" value="2">
+                                        <input type="radio" id="star2" name="rating" value="4">
                                         <label for="star2">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
@@ -128,13 +126,13 @@
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                         </label>
-                                        <input type="radio" id="star4" name="rating" value="4">
+                                        <input type="radio" id="star4" name="rating" value="2">
                                         <label for="star4">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                                             </svg>
                                         </label>
-                                        <input type="radio" id="star5" name="rating" value="5">
+                                        <input type="radio" id="star5" name="rating" value="1">
                                         <label for="star5">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16">
                                                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
